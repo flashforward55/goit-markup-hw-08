d22dda895b758b4093ec0ab7d00f47a339ea4b67
Anton Styazhkin <rudana089@gmail.com>
2023-03-22 23:55:34 +0200

    Add styles filetr.js

--- js/filter.js
+++ js/filter.js
@@ -9,9 +9,13 @@ buttons.forEach(button => {
       if (filter === 'all' || item.dataset.category === filter) {
         // item.style.display = 'block';
         item.classList.remove('hidden');
+        item.style.opacity = 1;
+        item.style.transition = 'opacity 0.9s ease-in-out, height 0.9s ease-in-out';
       } else {
         // item.style.display = 'none';
         item.classList.add('hidden');
+        item.style.opacity = 0;
+        item.style.transition = 'opacity 0.9s ease-in-out, height 0.9s ease-in-out';
       }
     });
   });
